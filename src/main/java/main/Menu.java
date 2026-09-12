cc31df352f2f06ca57a9da330ea5b834d628829d
Genesis-carvajal <genesis.carvajalsanchez@ucr.ac.cr>
2026-09-12 16:05:12 -0600

    Primer algoritmo

--- src/main/java/main/Menu.java
+++ src/main/java/main/Menu.java
@@ -4,10 +4,98 @@
  */
 package main;
 
+import arreglosprimitivos.Nivel1;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author carva
  */
 public class Menu {
-    
-}
+
+    private Nivel1 nivel1;
+    private int opcion;
+
+  public void MenuPrincipal() {
+    do {
+         opcion = Integer.parseInt(JOptionPane.showInputDialog("""
+                                                      BIENVENIDO
+
+                                                      seleccione una opción:
+                                                      1. ,,,
+                                                      2. ,,,
+                                                      3. ,,,
+                                                      4. ,,,
+                                                      5. ,,,
+                                                      6. ,,,
+
+                                                      """));
+
+    switch (opcion){
+        case 1:
+            nivel1.Decimales();
+        break;
+        case 2:
+            
+        break;        
+        case 3:
+        
+        break;        
+        case 4:
+        
+        break;        
+        case 5:
+        
+        break;        
+        case 6:
+        
+        break;   
+  
+        case 7:
+            
+        break;
+        case 8:
+            
+        break;        
+        case 9:
+        
+        break;        
+        case 10:
+        
+        break;        
+        case 11:
+        
+        break;        
+        case 12:
+        break;
+        case 13:
+            
+        break;
+        case 14:
+            
+        break;        
+        case 15:
+        
+        break;        
+        case 16:
+        
+        break;        
+        case 17:
+        
+        break;        
+        case 18:
+        
+        break;   
+  
+        case 19:
+            
+        break;
+        case 20:
+            
+        break;      
+        default:
+        
+        }//switch
+        }while (opcion !=20); //termina do-while
+    }//fin método
+}//fin de la clase
--- src/main/java/main/Principal.java
+++ src/main/java/main/Principal.java
@@ -11,6 +11,7 @@ package main;
 public class Principal {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Menu menu = new Menu();
+        menu.MenuPrincipal();
     }
 }
