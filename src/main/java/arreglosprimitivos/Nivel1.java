e086311991c1ba2fbb7122d199ab38c5b9454ce4
Genesis-carvajal <genesis.carvajalsanchez@ucr.ac.cr>
2026-09-11 19:05:47 -0600

    Segundo commit de Génesis

--- src/main/java/arreglosprimitivos/Nivel1.java
+++ src/main/java/arreglosprimitivos/Nivel1.java
@@ -9,5 +9,12 @@ package arreglosprimitivos;
  * @author carva
  */
 public class Nivel1 {
-    
+    //#1
+    public void Decimales() {
+        double[] decimales = {25.3, 28.52, 26.75, 32.32};
+        for (int i = 0; i < decimales.length; i++) {
+            System.out.println("Posicion " + i
+                    + ": " + decimales[i]);
+        }
+    }
 }
