9f95195a256c97444b081253fff66ddc7b2ce8f2
M0N5E <monserrath.salazarartavia@ucr.ac.cr>
2026-09-12 20:47:57 -0600

    Nivel1

--- src/main/java/arreglosprimitivos/Nivel1.java
+++ src/main/java/arreglosprimitivos/Nivel1.java
@@ -4,17 +4,53 @@
  */
 package arreglosprimitivos;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author carva
  */
 public class Nivel1 {
-    //#1
-    public void Decimales() {
-        double[] decimales = {25.3, 28.52, 26.75, 32.32};
-        for (int i = 0; i < decimales.length; i++) {
-            System.out.println("Posicion " + i
-                    + ": " + decimales[i]);
+        
+  /*  3 Básico int Leer 8 números enteros desde teclado y almacenarlos en un arreglo. Al
+finalizar, mostrar todos los valores uno por línea.
+Lectura, almacenamiento,recorrido
+*/
+    public void leerYMostrarNumeros() {
+
+        int[] numeros = new int[8];
+
+        for (int i = 0; i < 8; i++) {
+            String texto = JOptionPane.showInputDialog("Ingrese el numero " + (i + 1) + ":");
+            numeros[i] = Integer.parseInt(texto);
         }
+
+        String resultado = "Los numeros ingresados son:\n";
+        for (int i = 0; i < 8; i++) {
+            resultado += numeros[i] + "\n";
+        }
+
+        JOptionPane.showMessageDialog(null, resultado);
+    }
+
+   /* 4 Básico char Convertir una palabra conocida a un arreglo de caracteres y mostrar cada
+carácter por separado.
+char[], recorrido
+    */
+    public void ConvertirPalabra() {
+
+        String palabra = JOptionPane.showInputDialog("Ingrese una palabra:");
+        char[] letras = new char[palabra.length()];
+
+        for (int i = 0; i < palabra.length(); i++) {
+            letras[i] = palabra.charAt(i);
+        }
+
+        String resultado = "Los caracteres son:\n";
+        for (int i = 0; i < letras.length; i++) {
+            resultado += letras[i] + "\n";
+        }
+        JOptionPane.showMessageDialog(null, resultado);
+
     }
 }
--- src/main/java/main/Principal.java
+++ src/main/java/main/Principal.java
@@ -4,6 +4,8 @@
 
 package main;
 
+import arreglosprimitivos.Nivel1;
+
 /**
  *
  * @author carva
@@ -11,6 +13,9 @@ package main;
 public class Principal {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Nivel1 n1= new Nivel1();
+        //n1.leerYMostrarNumeros();
+        n1.ConvertirPalabra();
+        
     }
 }
